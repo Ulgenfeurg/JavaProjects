40c25e3d6fb7ad58e57625b0b2802dea50ff3f68
Ulgenfeurg <duhem@mail.ru>
2023-12-31 09:56:02 +0300

    final thing 1 and 2 updates works correct

--- shopping-list/src/Shopping.java
+++ shopping-list/src/Shopping.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Shopping {
     public static void main(String[] args){
         System.out.println("Вас приветствует список покупок!");
-        String[] shoppingList = new String[8];
+        //String[] shoppingList = new String[8];
+        ArrayList <String> shoppingList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         int productCount = 0;
 
@@ -20,13 +22,13 @@ public class Shopping {
                 System.out.println("Введите название товара:");
                 String productName = scanner.next();
                 boolean isExist = false;
-                if(productCount == 0){
-                    shoppingList[productCount] = productName;
+                if(productCount == 0){ //проверяем что товара нет на 0 месте,то есть массив пустой
+                    shoppingList.add(productName);
                     System.out.println("Товар " + productName + " добавлен в список под номером "+ (productCount +1));
                     productCount++;
                 } else{
-                    for(int i = 0; i < productCount;i++){
-                        if(shoppingList[i].equals(productName)){
+                    for(int i = 0; i < productCount;i++){ // проверяем что искомого товара нет в других местах массива
+                        if(shoppingList.get(i).equals(productName)){
                             isExist = true;
                             break;
                         } else {
@@ -37,28 +39,30 @@ public class Shopping {
                     if(isExist) {
                         System.out.println("Такой товар уже есть в списке!");
                     } else {
-                        shoppingList[productCount] = productName;
+                        shoppingList.add(productName);
                         System.out.println("Товар " + productName + " добавлен в список под номером "+ (productCount +1));
                         productCount++;
                     }
                 }
-                if(productCount >7){
+                /*if(productCount >7){
                     System.out.println("Список слишком большой, придётся отложить покупку до следующего раза");
-                }
+                    shoppingList.add(productName); - остатки от первоначального решения с ограничением размера
+                }*/
             } //добавить товар в список
             else if (actionNumber == 2){
                 for(int i=0;i<productCount; i++){
-                    System.out.println(i + 1 +". "+ shoppingList[i]);
+                    System.out.println(i + 1 +". "+ shoppingList.get(i));
                 }
                 if (productCount == 0){
                     System.out.println("Список пуст!");
                 }
             } // показать список
             else if (actionNumber == 3){
-                for(int i =0; i <= productCount; i++){
-                    shoppingList[productCount] = null;
-                }
+                /*for(int i =0; i <= productCount; i++){
+                    shoppingList.set(i, null);  -  тут я запутался в размерах массива и решил пойти простым путём
+                }*/
                 productCount = 0;
+                shoppingList.clear();
                 System.out.println("Список пуст!");
             } // очистить список
             else if (actionNumber == 4) { // завершить работу
@@ -66,7 +70,6 @@ public class Shopping {
             } else {
                 System.out.println("Неизвестная команда!");
             }
-
         }
     }
 }
