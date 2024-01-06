1aab2c22ef58cce6e33adbf4765fdea7de20bd09
Ulgenfeurg <duhem@mail.ru>
2024-01-06 11:50:18 +0300

    fix "if to switch" and "String in command" comments

--- shopping-list/src/Shopping.java
+++ shopping-list/src/Shopping.java
@@ -1,11 +1,9 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Shopping {
     public static void main(String[] args){
         System.out.println("Вас приветствует список покупок!");
-        //String[] shoppingList = new String[8];
-        ArrayList <String> shoppingList = new ArrayList<>();
+        String[] shoppingList = new String[8];
         Scanner scanner = new Scanner(System.in);
         int productCount = 0;
 
@@ -23,52 +21,53 @@ public class Shopping {
                     System.out.println("Введите название товара:");
                     String productName = scanner.next();
                     boolean isExist = false;
-                    if (productCount == 0) { //проверяем что товара нет на 0 месте,то есть массив пустой
-                        shoppingList.add(productName);
+                    if (productCount == 0) {
+                        shoppingList[0] = productName;
                         System.out.println("Товар " + productName + " добавлен в список под номером " + (productCount + 1));
                         productCount++;
                     } else {
-                        for (int i = 0; i < productCount; i++) { // проверяем что искомого товара нет в других местах массива
-                            if (shoppingList.get(i).equals(productName)) {
+                        int i;
+                        for (i = 0; i < productCount; i++) {
+                            if (shoppingList[i].equals(productName)) {
                                 isExist = true;
                                 break;
                             } else {
                                 isExist = false;
                             }
                         }
-                        //System.out.println(isExist);
                         if (isExist) {
                             System.out.println("Такой товар уже есть в списке!");
                         } else {
-                            shoppingList.add(productName);
+                            shoppingList[i] = productName;
                             System.out.println("Товар " + productName + " добавлен в список под номером " + (productCount + 1));
                             productCount++;
                         }
                     }
-                /*if(productCount >7){
-                    System.out.println("Список слишком большой, придётся отложить покупку до следующего раза");
-                    shoppingList.add(productName); - остатки от первоначального решения с ограничением размера
-                }*/
-                    //добавить товар в список
+                if(productCount > shoppingList.length-1){
+                    String [] shoppingList_2 = new String[productCount+1];
+                    for (int i = 0; i < shoppingList.length; i++) {
+                        shoppingList_2[i] = shoppingList[i];
+                    }
+                    shoppingList = shoppingList_2;
+                }
                     break;
                 case "2":
                     for (int i = 0; i < productCount; i++) {
-                        System.out.println(i + 1 + ". " + shoppingList.get(i));
+                        System.out.println(i + 1 + ". " + shoppingList[i]);
                     }
                     if (productCount == 0) {
                         System.out.println("Список пуст!");
                 } // показать список
                     break;
                 case "3":
-                /*for(int i =0; i <= productCount; i++){
-                    shoppingList.set(i, null);  -  тут я запутался в размерах массива и решил пойти простым путём
-                }*/
+                    for(int i =0; i <= productCount; i++){
+                        shoppingList[i] = null;
+                    }
                     productCount = 0;
-                    shoppingList.clear();
+                    //shoppingList.clear();
                     System.out.println("Список пуст!");
-                 // очистить список
                     break;
-                case "4": // завершить работу
+                case "4":
                     break;
                 default:
                     System.out.println("Неизвестная команда!");
